f43be34bf8e099682eecbf920b273fdb4450750a
noamriahi <noamriahi10@gmail.com>
2021-08-26 15:09:12 +0300

    ui

--- Towers Attack/Assets/Bank/Bank.cs
+++ Towers Attack/Assets/Bank/Bank.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startingBalance =150;
@@ -10,23 +10,33 @@ public class Bank : MonoBehaviour
     [SerializeField] int currentBalance;
     public int CurrentBalance { get { return currentBalance; } }
     
+    [SerializeField] TextMeshProUGUI displayBalance;
+
     void Awake() 
     {
+        
      currentBalance = startingBalance;   
+     UpdateDisplay();
     }
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
-
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
+        
         if(currentBalance<0)
         {
             ReloadScene();
         }
+    }
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
     void ReloadScene()
     {
