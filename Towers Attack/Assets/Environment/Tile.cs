a0df8b22eccd960241d5d3d85945debb5f7fb7fc
noamriahi <noamriahi10@gmail.com>
2021-09-05 09:31:33 +0300

    upgrade my path

--- Towers Attack/Assets/Environment/Tile.cs
+++ Towers Attack/Assets/Environment/Tile.cs
@@ -9,7 +9,26 @@ public class Tile : MonoBehaviour
     [SerializeField] bool isPlaceable;
     public bool IsPlaceable { get { return isPlaceable; } }
 
+    GridManager gridManager;
+    Vector2Int coordinates = new Vector2Int();
 
+    void Awake()
+    {
+        gridManager = FindObjectOfType<GridManager>();
+    }
+
+    void Start() 
+    {
+        if(gridManager != null)
+        {
+            coordinates = gridManager.GetCoordinateFromPosition(transform.position);
+
+            if(!isPlaceable)
+            {
+                gridManager.BlockedNode(coordinates);
+            }
+        }
+    }
     void OnMouseDown()
     {
         if(isPlaceable)
