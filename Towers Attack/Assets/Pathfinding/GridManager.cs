0171bfd56f2104b88bce271bfc249c12f9654c09
noamriahi <noamriahi10@gmail.com>
2021-09-09 19:22:44 +0300

    change walking steps

--- Towers Attack/Assets/Pathfinding/GridManager.cs
+++ Towers Attack/Assets/Pathfinding/GridManager.cs
@@ -34,6 +34,15 @@ public class GridManager : MonoBehaviour
             grid[coordinates].isWalkable = false;
         }
     }
+    public void ResetNodes()
+    {
+        foreach (KeyValuePair<Vector2Int , Node> entry in grid)
+        {
+            entry.Value.connectedTo = null;
+            entry.Value.isExplored = false;
+            entry.Value.isPath = false;          
+        }
+    }
     
     public Vector2Int GetCoordinateFromPosition(Vector3 position)
     {
--- Towers Attack/Assets/Pathfinding/Pathfinder.cs
+++ Towers Attack/Assets/Pathfinding/Pathfinder.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Pathfinder : MonoBehaviour
 {
     [SerializeField] Vector2Int startCoordinates;
+    public Vector2Int StartCoordinates {get {return startCoordinates; } }
     [SerializeField] Vector2Int destinationCoordinates;
+    public Vector2Int DestinationCoordinates {get {return destinationCoordinates; } }
 
     Node startNode;
     Node destinationNode;
@@ -24,6 +26,9 @@ public class Pathfinder : MonoBehaviour
         if(gridManager != null)
         {
             grid = gridManager.Grid;
+            startNode = grid[startCoordinates];
+            destinationNode = grid[destinationCoordinates];
+
         }
 
 
@@ -31,11 +36,13 @@ public class Pathfinder : MonoBehaviour
 
     void Start()
     {
-        startNode = gridManager.Grid[startCoordinates];
-        destinationNode = gridManager.Grid[destinationCoordinates];
-
+        GetNewPath();
+    }
+    public List<Node> GetNewPath()
+    {
+        gridManager.ResetNodes();
         BreadthFirstSearch();
-        BuildPath();
+        return BuildPath();
     }
 
     void ExploreNeighbors()
@@ -65,6 +72,13 @@ public class Pathfinder : MonoBehaviour
 
     void BreadthFirstSearch()
     {
+        startNode.isWalkable = true;
+        destinationNode.isWalkable = true;
+        
+        frontier.Clear();
+        reached.Clear();
+
+
         bool isRunning = true;
 
         frontier.Enqueue(startNode);
@@ -100,5 +114,24 @@ public class Pathfinder : MonoBehaviour
         path.Reverse();
 
         return path;
+    }
+    public bool WillBlockPath(Vector2Int coordinates)
+    {
+        if(grid.ContainsKey(coordinates))
+        {
+            bool previousState = grid[coordinates].isWalkable;
+
+            grid[coordinates].isWalkable = false;
+            List<Node> newPath = GetNewPath();
+            grid[coordinates].isWalkable = previousState;
+
+            if(newPath.Count <= 1)
+            {
+                GetNewPath();
+                return true;
+            }
+            
+        }
+        return false;
     }
 }
